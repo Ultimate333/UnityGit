56d4680f2448ba31b9f49222501ea96938c9e08b
Ultimate333 <belov.evgenijj333@gmail.com>
2025-07-27 18:47:15 +0300

    unity1

--- Assets/player.cs
+++ Assets/player.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-
-    // Update is called once per frame
+   
+    
     void Update()
     {
         
