7476a55eaf7b4bf23968e7bc7690206c2a034dfc
Ultimate333 <belov.evgenijj333@gmail.com>
2025-07-28 11:01:50 +0300

    движение игрока

--- Assets/coin.cs
+++ Assets/coin.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody2D))]
 public class coin : MonoBehaviour
 {
+    [Header("движение игрока")]
+    [SerializeField][Range(1f,10f)] private float speed = 5f;
     private Rigidbody2D rb;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    
+    void Start() => rb = GetComponent<Rigidbody2D>();
 
+   
+    
+    private void FixedUpdate()
+    {
+      float h =  Input.GetAxisRaw("Horizontal");
+     
+        rb.linearVelocity = new Vector2(h * speed, rb.linearVelocity.y);
+        
+    }
    
     }
 
