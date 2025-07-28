aa62781ce87c4f8fe2668205f3ec451cdec78bf5
Ultimate333 <belov.evgenijj333@gmail.com>
2025-07-28 11:40:31 +0300

    spawn enemy

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,5 +4,28 @@ using UnityEngine.SceneManagement;
 public class player : MonoBehaviour
 {
 
-    
+    [SerializeField] private GameObject objectToSpawn; // Префаб для спавна
+    [SerializeField] private Vector2 spawnAreaSize = new Vector2(5f, 5f); // Зона спавна
+
+    void Start()
+    {
+        // Спавн при старте
+        SpawnObject();
+    }
+
+    public void SpawnObject()
+    {
+        // Вычисление случайной позиции в зоне
+        Vector2 randomPosition = (Vector2)transform.position +
+            new Vector2(
+                Random.Range(-spawnAreaSize.x / 2, spawnAreaSize.x / 2),
+                Random.Range(-spawnAreaSize.y / 2, spawnAreaSize.y / 2)
+            );
+
+        // Создание объекта
+        GameObject newObj = Instantiate(objectToSpawn, randomPosition, Quaternion.identity);
+
+        // Автоудаление через 5 секунд
+        Destroy(newObj, 5f);
+    }
 }
